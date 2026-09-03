ee82d5ba2f2f3ceae16669ecc19e66a75e063644
imlast999 <lxstbrexthe@gmail.com>
2026-09-03 19:26:20 +0200

    added missing functions on libft.h

--- libft.h
+++ libft.h
@@ -115,5 +115,10 @@ void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
+char	**ft_split(char const *s, char c);
+int		split_str(char **split);
+void	free_split(char **split);
+int		count_all_numbers(char **av, int start);
+int		parsing_helper(t_stack *stack, char **split, int *i, int *error);
 
 #endif
